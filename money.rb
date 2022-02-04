ca91a2444e5e542ba40937044e6a4c21075d1ee6
TKG1 <kouheideal@gmail.com>
2022-02-04 13:03:06 +0900

    create: Ruby課題3_お金の種類分けを作成

---
+++ money.rb
@@ -0,0 +1,10 @@
+payment = ARGV[0].to_i.abs
+YEN = [ 1, 5, 10, 50, 100, 500, 1000, 5000, 10000 ]
+required_yen = []
+
+YEN.reverse.map{ |yen|
+  required_yen.prepend(payment / yen)
+  payment = payment % yen
+}
+
+p Hash[*[YEN, required_yen].transpose.flatten]
