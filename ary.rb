cfd6256e43c9f88b144a216f6996a046b6999dce
TKG1 <kouheideal@gmail.com>
2022-02-08 04:29:27 +0900

    modify:課題4 rankの代わりにwith_indexメソッドを使用

--- ary.rb
+++ ary.rb
@@ -1,10 +1,8 @@
 data = [["らんてくん", 98],["カンノ", 23],["ファラオ", 57],["だいそん", 84],["ひさじゅ", 100]]
-rank = 1
 
 # 整形処理のコード
 desc_data = data.sort_by(&:last).reverse
 
-desc_data.to_h.each do |name, score|
-  puts "#{rank}位: #{name} #{score}"
-  rank += 1
+desc_data.to_h.each.with_index(1) do |(name, score), index|
+  puts "#{index}位: #{name} #{score}"
 end
