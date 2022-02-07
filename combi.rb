474768af4a299a352cf23c7dd5818d2fc775d459
TKG1 <kouheideal@gmail.com>
2022-02-07 19:08:32 +0900

    modify: 課題2_combinationメソッドを無しで実装

--- combi.rb
+++ combi.rb
@@ -1,6 +1,17 @@
 arg = ARGV[0].to_i.abs
-array_numbers = []
+permutation_numbers = []
 
-1.upto(arg) {|i| array_numbers.push(i)}
+4.times do |i|
+  (1..arg).to_a.each do |x|
+    a = [i + 1, x].sort
+    permutation_numbers.push(a)
+  end
+end
 
-p array_numbers.combination(2).to_a
+permutation_numbers.delete_if { |num| num[0] == num[1] }
+combination_numbers = permutation_numbers.uniq
+
+p combination_numbers
+
+# 修正後
+# p (1..arg).to_a.combination(2).to_a
